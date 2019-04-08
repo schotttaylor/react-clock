4b90940f532a3269251bbe05b719d436f0f6c93c
Schott Taylor <staylor2@netspend.com>
2019-04-08 09:57:33 -0500

    Added state updates for getting the date

--- src/components/date/Date.tsx
+++ src/components/date/Date.tsx
@@ -4,20 +4,43 @@ import './Date.css';
 
 export default class DateDisplay extends Component {
 
+  state = {
+    dayVal: '',
+    monthVal: '',
+    yearVal: ''
+  };
+
   monthNames = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-  time     = new Date();
-  monthVal = this.monthNames[this.time.getMonth()];
-  dayVal   = ("0" + this.time.getDate()).slice(-2);
-  yearVal  = this.time.getFullYear().toString().substr(2, 2);
+
+  constructor(props: {}) {
+    super(props);
+    this.getDate = this.getDate.bind(this);
+  }
+
+  componentDidMount() {
+    this.getDate()
+  }
+
+  getDate() {
+    const now = new Date();
+    
+    this.setState({ 
+      dayVal   : ("0" + now.getDate()).slice(-2),
+      monthVal : this.monthNames[now.getMonth()],
+      yearVal  : now.getFullYear().toString().substr(2, 2),
+    })
+    
+    requestAnimationFrame(this.getDate)
+  }
 
   render() {
     return (
       <Fragment>
         <div className="date">
           <div className="dateDisplay">
-            <span className="month">{this.monthVal}</span>
-            <span className="day">{this.dayVal}</span>
-            <span className="year">{this.yearVal}</span>
+            <span className="month">{this.state.monthVal}</span>
+            <span className="day">{this.state.dayVal}</span>
+            <span className="year">{this.state.yearVal}</span>
           </div>
         </div>
       </Fragment>
