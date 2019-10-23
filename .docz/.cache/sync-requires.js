fe99a4096a7a06c07716d956269229ba5cb1976e
Schott Taylor <schotttaylor@gmail.com>
2019-10-23 16:40:01 -0500

    Updates component pattern

--- .docz/.cache/sync-requires.js
+++ .docz/.cache/sync-requires.js
@@ -7,6 +7,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---readme-mdx": hot(preferDefault(require("/Users/schott/projects/react-clock/README.mdx"))),
   "component---src-index-mdx": hot(preferDefault(require("/Users/schott/projects/react-clock/src/index.mdx"))),
+  "component---src-components-numbers-numbers-mdx": hot(preferDefault(require("/Users/schott/projects/react-clock/src/components/numbers/numbers.mdx"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/schott/projects/react-clock/.docz/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/schott/projects/react-clock/.docz/src/pages/404.js")))
 }
---
+++ src/app/app.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import './app.css';
+import { DateDisplay } from 'src/components/date';
+import { Face } from 'src/components/face';
+import { Hands } from 'src/components/hands';
+import { Numbers } from 'src/components/numbers';
+import { Ticks } from 'src/components/ticks';
+import { Title } from 'src/components/title';
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <Title/>
+      <div className="clock">
+        <Face />
+        <DateDisplay />
+        <Numbers />
+        <Ticks />
+        <Hands />
+      </div>
+    </div>
+  );
+}
+
