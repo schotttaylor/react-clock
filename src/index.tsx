18f02a6df929f533e83a5ae94579d1332ca9f209
Schott Taylor <schotttaylor@gmail.com>
2025-01-12 22:14:58 -0700

    Fixing github pages deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import registerServiceWorker from './registerServiceWorker';
 import { App } from './app';
 import './index.css';
-
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
+const container = document.getElementById('root');
+const root = createRoot(container!);
 root.render(<App />);
 
 registerServiceWorker();
